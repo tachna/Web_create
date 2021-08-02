59e9a62317da7ebf7f5a479bc5df95e6198adc3d
touch <jo8020173@gmail.com>
2021-08-02 16:29:33 +0900

    realrecent

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ app.use(express.urlencoded({extended: true}))
 const MongoClient = require('mongodb').MongoClient;//mongodb 사용
 app.set('view engine', 'ejs');
 app.use('/public', express.static('public')) //미들웨어, public폴더를 사용할것이다 (css파일관련)
+app.use('/uploads', express.static('uploads'));
+
 
 var db;
 MongoClient.connect('mongodb+srv://cho1:jk007456@cluster0.ikeip.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',function(err, client){
@@ -18,6 +20,15 @@ MongoClient.connect('mongodb+srv://cho1:jk007456@cluster0.ikeip.mongodb.net/myFi
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/homepage.html')
+});
+app.get('/list', function(req, res){
+    res.sendFile(__dirname + '/list.html')
+});
+app.get('/install', function(req, res){
+    res.sendFile(__dirname + '/install.html')
+});
+app.get('/login', function(req, res){
+    res.sendFile(__dirname + '/login.html')
 });
 
 
