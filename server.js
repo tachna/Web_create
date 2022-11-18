3f945b85f8f31dbaf92f52f89fbf88deb04ef4bc
chikako_2426 <78514962+kimikitae@users.noreply.github.com>
2022-11-18 12:29:18 +0900

    회원가입 수정

--- server.js
+++ server.js
@@ -185,7 +185,7 @@ passport.deserializeUser(function(아이디, done){
 
 // register로 post요청하면 db에 로그인 정보 추가
 app.post('/register', function(req,res){
-    db.collection('login').insertOne({ id: req.body.id, pw: req.body.pw, _id: req.body.id, born: req.body.born, sex: req.body.sex, address: req.body.address, number: req.body.number, email: req.body.email
+    db.collection('login').insertOne({ id: req.body.id, pw: req.body.pw, _id: req.body.id, born: req.body.born, sex: req.body.sex, address: req.body.address, number: req.body.number, email: req.body.email, edomain: req.body.edomain, allow: req.body.allow
     }, function(err,result){
         res.redirect('/login')
     })
